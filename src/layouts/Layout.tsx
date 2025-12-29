ba95238ad5a2831d62cf5a264a6466a82018ac49
Tae.Garam <drasla@naver.com>
2025-12-29 10:50:45 +0900

    header 디자인 완료

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header.tsx";
 
 function Layout() {
     return (
-        <div className={twMerge("min-h-screen", "pt-14")}>
+        <div className={twMerge("min-h-calc[100dvh-var(--height-header)]", "pt-14")}>
             <Header />
             <main className={"p-4"}>
                 <Outlet />
